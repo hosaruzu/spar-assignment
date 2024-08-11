56c08b41c665352e30785bfea46ebd36f9b5f4cb
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-08-11 12:28:39 +0300

    Init project

---
+++ spar-assignment/spar-assignment/spar_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  spar_assignmentApp.swift
+//  spar-assignment
+//
+//  Created by Artem Tebenkov on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct spar_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
